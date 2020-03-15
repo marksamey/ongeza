04626de65dee19d53ea59f68ff84004067b2adf1
Mohammed Baruti <marksamey@gmail.com>
2020-03-16 01:50:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
 </head>
